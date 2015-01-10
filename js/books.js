6f813bb6f88fd3d07fa593696d38fbdecf30cf21
prashant andani <prashant.andani@careervita.com>
2015-01-10 16:50:45 +0530

    working prototype

--- js/books.js
+++ js/books.js
@@ -1,39 +1,87 @@
 var app = angular.module('books', []);
+var priceServiceApiKey = '659b4b387abab6206c7955370cc01ef85b75a875';
+app.factory('bookService',['$rootScope','$facebook',function($rootScope, $facebook){
+  var bookService = {};
+  bookService.user_books = [];
+  bookService.wants_to_read = {};
+  bookService.liked_books = {};
+  bookService.getFBUserBooks = function ($scope) {
+    //get books that i wanted to read
+    var instance_wants_to_read =  {};
+    $facebook.cachedApi('/me/books.wants_to_read').then(function(books) {
+      $rootScope.$broadcast('wants_to_read_books', books);
 
-app.directive('searchView', ['$http', function($http, $scope){
-        
-    return {
-      restrict: 'EA',
-      templateUrl : 'partials/bookslist.html',
-      controller: function($scope){
-        $scope.is_data = '';
-        $scope.keyword = 'java';
-        $scope.books = '';
-        $scope.base_url = window.location.host;
-        $scope.getBooks = function(){
-           $http.get('https://www.googleapis.com/books/v1/volumes?q='+$scope.keyword).success(
-            function(data){
-              //$scope.books = data;
-                if(data){
-                  $scope.books = data;
-                  $scope.is_data = true;                  
-                }else{
-                  $scope.is_data = false;
-                  $scope.books = '';
-                }
-            });
-        }
-        $scope.getBooks();
+    });
+    //get books that user liked
+    $facebook.cachedApi('/me/books').then(function(books) {
+      $rootScope.$broadcast('liked_books', books);
+    });
+    // /this.wants_to_read = instance_wants_to_read;
+    
+  };
+
+  return bookService;
+}]);
+
+app.factory('priceService',['$rootScope','$http',function($rootScope, $http){
+  var priceService = {};
+  priceService.getBookPrice = function(isbn){
+    $http.get('http://api.dataweave.in/v1/book_search/searchByIsbn/?api_key='+priceServiceApiKey+'&isbn='+isbn).success(
+    function(data){
+      if(data){
+        console.log(data);
+        $rootScope.$broadcast('priceBroadcast', data);            
+      }else{
+        //$scope.is_data = false;
+        //$scope.book = '';
+      }
+    });
+  }
+  return priceService;
+}]);
+
+app.controller('mainCtrl', ['$scope', '$facebook', function($scope, $facebook) {
+    $scope.$on('fb.auth.authResponseChange', function() {
+      $scope.status = $facebook.isConnected();
+      if($scope.status) {
+        $facebook.api('/me').then(function(user) {
+          $scope.user = user;
+        });
+        //$scope.getUserBooks();
        
-      },
-      
-    }
+      }
+    });    
+   
+    $scope.loginToggle = function() {
+      if($scope.status) {
+        $facebook.logout();
+      } else {
+        $facebook.login();
+      }
+    };
+
+    $scope.getUserBooks = function() {}
+
   }]);
 
-app.controller('bookController', ['$http','$scope', '$routeParams', function($http, $scope, $routeParams){
+app.controller('bookController', ['$http','$scope', '$routeParams','priceService', function($http, $scope, $routeParams, priceService){
   $scope.book = '';
   $scope.is_data = false;
+  $scope.is_price_available = false;
   $scope.volume_id = $routeParams.volume;
+  $scope.isbn = $routeParams.isbn;
+  $scope.book_price_list = '';
+  if($scope.isbn){
+    priceService.getBookPrice($scope.isbn);
+  }
+  $scope.$on('priceBroadcast', function(event, price_data){
+      console.log("listening the broadcast");
+      if(price_data.status_code == 200){
+        $scope.is_price_available = true;
+        $scope.book_price_list = price_data.data;
+      }
+  });
+
   $http.get('https://www.googleapis.com/books/v1/volumes/'+$scope.volume_id).success(
     function(data){
       if(data){
@@ -45,6 +93,62 @@ app.controller('bookController', ['$http','$scope', '$routeParams', function($ht
       }
     });
 }]);
+
+app.directive('searchView', ['$http','$rootScope','bookService', function($http, $rootScope, bookService){
+        
+    return {
+      restrict: 'EA',
+      templateUrl : 'partials/bookslist.html',
+      controller: function($scope, bookService){
+        $scope.is_data = '';
+        $scope.keyword = '';
+        $scope.books = '';
+        $scope.fb_books = [];
+        $scope.base_url = window.location.host;
+
+        bookService.getFBUserBooks();
+        $scope.getBooks = function(keyword){
+            var user_keyword = keyword;
+            if(keyword || $scope.keyword == ''){
+              user_keyword = keyword;
+            }
+            else{
+              user_keyword = $scope.keyword;
+            }
+           $http.get('https://www.googleapis.com/books/v1/volumes?q='+user_keyword).success(
+            function(data){ 
+              //$scope.books = data;
+                if(data){
+                  $scope.books = data;
+                  $scope.is_data = true;                 
+                }else{
+                  $scope.is_data = false;
+                  $scope.books = '';
+                }
+            });
+        }
+        $scope.$on('wants_to_read_books',function(event, books){
+          $.each(books.data, function(index, book){            
+            $scope.fb_books.push(book.data.book.title);
+            $scope.getBooks(book.data.book.title);
+          })
+        });
+
+        $scope.$on('liked_books',function(event, books){          
+          $.each(books.data, function(index, book){            
+            $scope.fb_books.push(book.name);
+            $scope.getBooks(book.name);
+
+          })
+        });
+
+
+        
+       
+      },
+      
+    }
+  }]);
 
 function callback(data) {
   console.log(data);
--- js/main.js
+++ js/main.js
@@ -1,17 +1,29 @@
-
- var app = angular.module('googleBooks', [
-  'ngRoute','books'
+var app = angular.module('googleBooks', [
+  'ngRoute','books','ngFacebook'
 ]);
 
 
 var base_url = window.location.origin;
 
-app.config(['$routeProvider','$httpProvider', function ($routeProvider, $httpProvider) {
-   
-  $routeProvider
+app.config(['$facebookProvider','$routeProvider','$httpProvider', function ($facebookProvider, $routeProvider, $httpProvider) {
+ 	$facebookProvider.setAppId('337747966330689').setPermissions(['email','user_friends','user_actions.books']);
+	$routeProvider
     .when("/", {templateUrl: "partials/home.html"})
     .when("/book", {templateUrl: "partials/book.html", controller: "bookController"})
    
 }]);
 
+
+app.run(['$rootScope', '$window', function($rootScope, $window) {
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    $rootScope.$on('fb.load', function() {
+      $window.dispatchEvent(new Event('fb.load'));
+    });
+  }]);
 
